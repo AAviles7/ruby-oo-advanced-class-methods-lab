42a346cadd0aa47e06be989fb0975784225b9a8d
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-14 23:31:57 -0500

    commit tree 0ac239d6a0f34e4b3509840e48c116201c59a5f5

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,51 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @@all.push(self)
+    return self
+  end
+
+  def self.new_by_name(name)
+    @name = name
+  end
+
+  def self.create_by_name(name)
+    @name = name
+    @@all.push(self)
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| (song.name == name)}
+  end
+
+  def self.find_or_create_by_name(name)
+    x = 0
+    @@all.length.times do
+      if @@all[x].name == name
+        return @@all[x]
+      end
+      x += 1
+    end
+    @name = name
+    @@all.push(self)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|songs| songs.name}
+  end
+
+  def self.new_from_filename
+    @@all
+  end
+
+  def self.create_from_filename
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+
 end
